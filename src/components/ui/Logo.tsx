1e4dfd94dc2193faaba6520f8c77cbe7ca3f9ce4
paperbase1 <peakprepapp@email.com>
2026-04-30 16:02:31 +0530

    fix: refine Logo SVG to perfectly match asymmetrical user design

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -5,19 +5,15 @@ export const Logo = ({ size = 24, className = "" }: { size?: number, className?:
     <svg 
       width={size} 
       height={size} 
-      viewBox="0 0 1024 1024" 
+      viewBox="0 0 100 100" 
       fill="none" 
       xmlns="http://www.w3.org/2000/svg"
       className={className}
     >
       <path 
-        d="M221.3 543.1C221.3 400.1 336.9 284.5 480 284.5C623.1 284.5 738.7 400.1 738.7 543.1V714.7H221.3V543.1Z" 
-        fill="currentColor" 
-      />
-      <path 
         fillRule="evenodd" 
         clipRule="evenodd" 
-        d="M221.3 714.7H738.7V543.1C738.7 400.1 623.1 284.5 480 284.5C336.9 284.5 221.3 400.1 221.3 543.1V714.7ZM141.3 543.1C141.3 356.1 292.9 204.5 480 204.5C667.1 204.5 818.7 356.1 818.7 543.1V794.7H141.3V543.1Z" 
+        d="M20 75C20 45 40 25 70 25C90 25 90 45 90 75H20ZM30 65C30 50 40 38 55 38C65 38 72 45 72 65H30Z" 
         fill="currentColor" 
       />
     </svg>
